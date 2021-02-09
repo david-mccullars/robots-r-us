0023cc25b3f8f887cb3f4b36b1ad25afcb098375
David McCullars <david@bloomfire.com>
2021-02-08 18:30:34 -0600

    Uglify data from mock API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import data from './data';
+import { data, fields, field_types } from './data.json';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
-      {/* {data.map(robot => ...)} */}
+      {/* {data.map(employee => ...)} */}
     </div>
   );
 }
